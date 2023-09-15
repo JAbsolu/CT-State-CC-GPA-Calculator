7631817b3bd22c15d1bb406088866bb7714868f6
Johnson Absolu <Johnson.absolu@outlook.com>
2023-09-15 00:57:54 -0400

    updated js script

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,5 @@
 // GET ALL COURSE SECTION FIELDS
-const courseEl = document.querySelectorAll("course");
+const course = document.querySelectorAll("course");
 const grade1 = document.getElementsByClassName("grade1");
 const grade2 = document.getElementsByClassName("grade2");
 const grade3 = document.getElementsByClassName("grade3");
@@ -8,7 +8,7 @@ const grade5 = document.getElementsByClassName("grade5");
 const grade6 = document.getElementsByClassName("grade6");
 const grade7 = document.getElementsByClassName("grade7");
 const grade8 = document.getElementsByClassName("grade8");
-const creditsEl = document.getElementsByClassName("credits");
+const credits = document.getElementsByClassName("credits");
 const isRepeated1 = document.getElementsByClassName("repeated1");
 const isRepeated2 = document.getElementsByClassName("repeated2");
 const isRepeated3 = document.getElementsByClassName("repeated3");
@@ -16,7 +16,7 @@ const isRepeated5 = document.getElementsByClassName("repeated5");
 const isRepeated6 = document.getElementsByClassName("repeated6");
 const isRepeated7 = document.getElementsByClassName("repeated7");
 const isRepeated8 = document.getElementsByClassName("repeated8");
-const currentGpaEl = document.getElementsByClassName("current-gpa");
+const currentGpa = document.getElementsByClassName("current-gpa");
 const totalExistingCredits = document.getElementsByClassName("current-total-credits");
 
 // GET CURRENT CREDITS & GPA
@@ -24,4 +24,21 @@ const currentGpaVal = currentGpaEl.value();
 const totalCredits = totalExistingCredits.value();
 
 //GET GRADE VALUE ON SELECTION
-// const getGrade
+const getGradesVal = () => {
+    
+    let grade1Val = grade1.value;
+    let grade2Val = grade2.value;
+    let grade3Val = grade3.value;
+    let grade4Val = grade4.value;
+    let grade5Val = grade5.value;
+    let grade6Val = grade6.value;
+    let grade7Val = grade7.value;
+    let grade8Val = grade8.value;
+
+    for (let val of grade1) {
+        grade1Val = val;
+    }
+
+    alert(grade1Val)
+
+}
