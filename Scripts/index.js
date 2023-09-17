cb60a7f3900b3d47d2dd575438e8ba7cce8ed648
Johnson Absolu <Johnson.absolu@outlook.com>
2023-09-17 02:37:00 -0400

    Added basic function to calculate GPA

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,67 +1,87 @@
 // GET ALL COURSE SECTION FIELDS
-const course = document.querySelectorAll("course");
-const grade1 = document.getElementsByClassName("grade1");
-const grade2 = document.getElementsByClassName("grade2");
-const grade3 = document.getElementsByClassName("grade3");
-const grade4 = document.getElementsByClassName("grade4");
-const grade5 = document.getElementsByClassName("grade5");
-const grade6 = document.getElementsByClassName("grade6");
-const grade7 = document.getElementsByClassName("grade7");
-const grade8 = document.getElementsByClassName("grade8");
-const credits = document.getElementsByClassName("credits");
-const isRepeated1 = document.getElementsByClassName("repeated1");
-const isRepeated2 = document.getElementsByClassName("repeated2");
-const isRepeated3 = document.getElementsByClassName("repeated3");
-const isRepeated5 = document.getElementsByClassName("repeated5");
-const isRepeated6 = document.getElementsByClassName("repeated6");
-const isRepeated7 = document.getElementsByClassName("repeated7");
-const isRepeated8 = document.getElementsByClassName("repeated8");
+let course = document.querySelectorAll(".course");
+let gradeElements = document.querySelectorAll('select[name="grade"]');
+let creditsElements = document.querySelectorAll('.gpaCredits');
 const currentGpa = document.getElementsByClassName("current-gpa");
 const gpaScore = document.querySelector(".gpa-score");
 const totalExistingCredits = document.getElementsByClassName("current-total-credits");
+const calculate = document.querySelector(".calculate");
 
-//GET GRADE VALUE ON SELECTION
 
+document.addEventListener('DOMContentLoaded', function() {
+    // PREVENT FORM FROM RELAODING
+    const form = document.querySelector('#prevent-default');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    })
+    // ADD NEW CLASS ROW
+    let rowCount = 5
+    const addRow = document.querySelector('.btn-add-row');
+    const addClassRow = () => { 
+        const gpaFormDiv = document.querySelector('.gpaFormDiv');
+        const newRow = ` 
+        <div class="addCourseRow"> 
+            <label for="class">Class ${rowCount}</label>
+            <input type="text" name="class" placeholder="Course">
+            <label for="grade">Grade</label>
+            <select id="grade" name="grade">
+                <option value="4">A</option>
+                <option value="3.7">A--</option>
+                <option value="3.3">B+</option>
+                <option value="3">B</option>
+                <option value="2.7">B-</option>
+                <option value="2.3">C+</option>
+                <option value="2">C</option>
+                <option value="1.7">C-</option>
+                <option value="1.3">D+-</option>
+                <option value="1">D</option>
+                <option value="0.7">D-</option>
+                <option value="0">F</option>
+            </select>
+            <span>
+                <label for="credits">Credits</label>
+                <input type="text" name="credits" class="gpaCredits">
+            </span>
+        </div>
+        <br/>
+        `
+        rowCount += 1
+        gpaFormDiv.innerHTML += newRow
+    
+        console.log("class row added")
+    }
+    addRow.addEventListener('click', addClassRow); 
 
-// PREVENT FORM FROM RELAODING
-const form = document.querySelector('#prevent-default');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-})
-// ADD NEW CLASS ROW
-let rowCount = 5
-const addRow = document.querySelector('.btn-add-row');
-const addClassRow = () => { 
-    const gpaFormDiv = document.querySelector('.gpaFormDiv');
-    const newRow = ` 
-    <div class="addCourseRow"> 
-        <label for="class">Class ${rowCount}</label>
-        <input type="text" name="class" placeholder="Course">
-        <label for="grade">Grade</label>
-        <select id="grade" name="grade">
-            <option value="4">A</option>
-            <option value="3.7">A--</option>
-            <option value="3.3">B+</option>
-            <option value="3">B</option>
-            <option value="2.7">B-</option>
-            <option value="2.3">C+</option>
-            <option value="2">C</option>
-            <option value="1.7">C-</option>
-            <option value="1.3">D+-</option>
-            <option value="1">D</option>
-            <option value="0.7">D-</option>
-            <option value="0">F</option>
-        </select>
-        <span>
-            <label for="credits">Credits</label>
-            <input type="text" name="credits" class="gpaCredits">
-        </span>
-    </div>
-    <br/>
-    `
-    rowCount += 1
-    gpaFormDiv.innerHTML += newRow
+    // Add an event listener to the button
+    calculate.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+  
+      let totalPoints = 0;
+      let totalCredits = 0;
+      let totalCourses = 0;
 
-    console.log("class row added")
-}
-addRow.addEventListener('click', addClassRow); 
+      course.forEach(function(course) {
+        totalCourses += 1;
+      })
+
+      alert(totalCourses)
+  
+      // Iterate through the grade elements
+      gradeElements.forEach(function(gradeElement, index) {
+        var gradeValue = parseFloat(gradeElement.value);
+        var creditsValue = parseFloat(creditsElements[index].value);
+  
+        if (!isNaN(gradeValue) && !isNaN(creditsValue)) {
+          totalPoints += gradeValue * creditsValue;
+          totalCredits += creditsValue;
+        }
+      });
+  
+      if (totalCredits > 0) {
+        var gpa = totalPoints / totalCredits;
+        console.log('Your GPA is:', gpa);
+      } else {
+        console.log('No valid grades or credits provided.');
+      }
+    });
+  });
